77c0c33de85bc69fa6ccdf3d389185ec80aa74ce
Bongomin Daniel <bongomindaniel@gmail.com>
2023-11-07 19:45:10 +0300

    create user Model

--- userAuth/models.py
+++ userAuth/models.py
@@ -1,5 +1,24 @@
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
 from django.utils import timezone
+from .managers import UserManager
 
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    date_joined = models.DateTimeField(default=timezone.now)
 
+    is_active = models.BooleanField(default=False)
+
+    is_staff = models.BooleanField(default=False)
+
+    # Additional fields for user roles
+    is_admin = models.BooleanField(default=False)
+    is_general_user = models.BooleanField(default=True)
+    is_other_role = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
