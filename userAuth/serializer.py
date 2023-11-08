35344aaa795e3eccb338c2b9f7c7805b2f76b676
Bongomin Daniel <bongomindaniel@gmail.com>
2023-11-08 04:55:33 +0300

    user serializer

---
+++ userAuth/serializer.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from .models import CustomUser
+
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['email', 'first_name', 'last_name', 'is_staff', 'is_general_user', 'is_active']
+        order = ['-date_joined']
