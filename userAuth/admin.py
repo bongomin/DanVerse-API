7a3f515e710b600ffcd244bfcf9059c63fd05198
Bongomin Daniel <bongomindaniel@gmail.com>
2023-11-08 04:55:19 +0300

    structure the admin view

--- userAuth/admin.py
+++ userAuth/admin.py
@@ -1,4 +1,27 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
 from .models import CustomUser
 
-admin.site.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('email', 'username', 'first_name', 'last_name', 'is_staff', 'is_general_user', 'is_superuser', 'is_active')
+    list_filter = ('is_staff', 'is_superuser', 'is_active')
+
+    list_editable = ('is_active', 'is_staff', 'is_superuser')
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (None, {'fields': ('first_name', 'last_name')}),
+        (None, {'fields': ('is_active', 'is_staff', 'is_superuser', 'is_admin', 'is_general_user', 'is_other_role')}),
+    )
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (None, {'fields': ('first_name', 'last_name')}),
+        (None, {'fields': ('is_active', 'is_staff', 'is_superuser', 'is_admin', 'is_general_user', 'is_other_role')}),
+    )
+
+    search_fields = ('email', 'first_name', 'last_name')
+    ordering = ('email',)
+
+admin.site.register(CustomUser, CustomUserAdmin)
