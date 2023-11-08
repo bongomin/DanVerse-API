84fc37bea19ea07b11a05920c76fbd8ff9815739
Bongomin Daniel <bongomindaniel@gmail.com>
2023-11-08 13:07:42 +0300

    add secret key to .env file

--- dan_verse_api/settings.py
+++ dan_verse_api/settings.py
@@ -1,5 +1,7 @@
 
 from pathlib import Path
+import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -9,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9@$+9r8rv_)u&&z$s$gwa)bi=-wijst&k9k_y%8rm!py&g#7+@'
+SECRET_KEY = 'os.environ.get("SECRET_KEY")'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -75,6 +77,17 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'railway',
+#         'USER': 'postgres',
+#         'PASSWORD': 'EGMsnnLGQuzQpRBGyKUG',
+#         'HOST': 'containers-us-west-76.railway.app',
+#         'PORT': '5457',
+#     }
+# }
 
 
 # Password validation
